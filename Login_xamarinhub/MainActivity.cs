17f4f657b04c26d135fbba03e2fd4aed737bc567
vijil <vijilkmrv2@gmail.com>
2016-10-21 23:31:51 +0530

    baseline

--- Login_xamarinhub/MainActivity.cs
+++ Login_xamarinhub/MainActivity.cs
@@ -5,27 +5,68 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading;
+using Android.Graphics;
 
 namespace Login_xamarinhub
 {
-    [Activity(Label = "Login_xamarinhub", MainLauncher = true, Icon = "@drawable/icon")]
+    
+    [Activity(Label = "Login_xamarinhub", MainLauncher = true, Icon = "@drawable/icon",Theme = "@android:style/Theme.Material.Light")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
+        private Button btnsignin;
+        
+        private EditText edtemail, edtnumber;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
+            btnsignin = (Button)FindViewById(Resource.Id.btnsign);
+            edtemail = (EditText)FindViewById(Resource.Id.txtemail);
+            edtnumber = (EditText)FindViewById(Resource.Id.txtnumber);
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            edtemail.TextChanged += Edtemail_TextChanged;
+            edtnumber.TextChanged += Edtnumber_TextChanged;
+            btnsignin.Click += Btnsignin_Click1;
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
         }
+
+        private void Edtnumber_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            if (edtnumber.Text.Length <= 0)
+            {
+                RunOnUiThread(() => {
+                    edtnumber.Hint = "Required...";
+
+                });
+            }
+        }
+
+        private void Edtemail_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            if (edtemail.Text.Length <= 0)
+            {
+                RunOnUiThread(() => {
+                    edtemail.Hint = "Required...";
+
+                });
+            }
+        }
+
+        private void Btnsignin_Click1(object sender, EventArgs e)
+        {
+            if((edtemail.Text.Length !=0 )&&(edtnumber.Text.Length !=0))
+            { 
+            Toast.MakeText(this, "Login Success", ToastLength.Long).Show();
+            }
+        }
+
+        
     }
 }
 
--- Login_xamarinhub/Resources/Resource.Designer.cs
+++ Login_xamarinhub/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace Login_xamarinhub
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int Sty_otherloginbox = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,8 +63,44 @@ namespace Login_xamarinhub
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f05000b
+			public const int btnfb = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int btngoogle = 2131034124;
+			
+			// aapt resource value: 0x7f050007
+			public const int btnsign = 2131034119;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int linearLayout2 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int linearLayout3 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int linearLayout4 = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int linearLayout5 = 2131034122;
+			
+			// aapt resource value: 0x7f050001
+			public const int txtemail = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int txtforgotpassword = 2131034117;
+			
+			// aapt resource value: 0x7f050009
+			public const int txtgoogle = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int txtnumber = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int view1 = 2131034114;
 			
 			static Id()
 			{
